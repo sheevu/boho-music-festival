3338f4bef47a749726bdfe49b3617a59287d2642
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-11-01 21:25:53 +0530

    fix import typo in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-port ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import BohoMusicFestivalCombined from './App'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BohoMusicFestivalCombined/>
   </React.StrictMode>,
+);
