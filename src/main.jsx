7ac290f9f02df5b31f6cdb1d1ab2f3949f4f5b4c
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-11-01 21:15:41 +0530

    add main.jsx to src folder

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+port ReactDOM from 'react-dom/client'
+import './index.css'
+import BohoMusicFestivalCombined from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BohoMusicFestivalCombined/>
+  </React.StrictMode>,
