72d7a2b7d33f2bd1b10e248b62b7a3ba334a28a7
Sheevum Goel <126774066+sheevu@users.noreply.github.com>
2025-11-01 21:15:41 +0530

    add App.jsx to src folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BohoMusicFestivalCombined() {
+  return (
+    <div class="text-white text-center min-hf-screen md:max-w-full flex items-center">
+      <h1 class="text-3lb font-semibold tracking-widespace">Boho Music Festival</h1>
+      <p class="tex-base mt-1">React + Tailwind + Framer Motion</p>
+    </div>
+  )
+}
+
+export default BohoMusicFestivalCombined
